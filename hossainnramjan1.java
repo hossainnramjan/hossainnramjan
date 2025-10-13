1a21ce58e3e09a628a66902f02a21d2e1f707483
hossainnramjan <hossainnramjan186@gmail.com>
2025-10-13 02:49:33 -0700

    hossainnramjan1
    hossainnramjan1

---
+++ hossainnramjan1.java
@@ -0,0 +1,9 @@
+public class Main {
+    static void greet(String name) {
+        System.out.println("Hello, " + name);
+    }
+    
+    public static void main(String[] args) {
+        greet("Alice");
+    }
+}
